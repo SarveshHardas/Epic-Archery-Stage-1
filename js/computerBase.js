e8e190574f9cd4061ea41dcea154284a5c4bc4ea
SarveshHardas <86554291+SarveshHardas@users.noreply.github.com>
2021-07-23 07:11:38 +0530

    Add files via upload

---
+++ js/computerBase.js
@@ -0,0 +1,28 @@
+class ComputerBase
+{
+    constructor(x,y)
+    {
+       
+        this.x=x
+        this.y=y
+        this.radius=150
+        var options={
+            isStatic:true
+        }
+        this.body=Bodies.circle(x,y,this.radius,options)
+        this.image=loadImage("assets/base2.png")
+        World.add(world,this.body)
+    }
+    display()
+    {
+        var pos = this.body.position
+        var angle = this.body.angle
+        push()
+        translate(pos.x,pos.y)
+        rotate(angle)
+        imageMode(CENTER)
+        image(this.image,0,0,this.radius,this.radius)
+        pop ()
+
+    }
+}
